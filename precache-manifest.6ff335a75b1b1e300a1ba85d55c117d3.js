3ac7bf06358ab9b867c762dd281d38599141430c
Betty <s9654025@gmail.com>
2019-07-09 13:14:38 +0800

    Updates

---
+++ precache-manifest.6ff335a75b1b1e300a1ba85d55c117d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5092dadcaabb9690e4fc3b7544bd21d4",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "b8ed2d413ca4bf5b37e6",
+    "url": "/pomodoro/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pomodoro/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b8ed2d413ca4bf5b37e6",
+    "url": "/pomodoro/static/js/main.e8056164.chunk.js"
+  },
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro/static/media/logo.5d5d9eef.svg"
+  }
+]);
