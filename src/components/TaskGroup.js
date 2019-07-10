032b1856cebd3bc0948d052a790bd985f678f733
Betty <s9654025@gmail.com>
2019-07-10 22:12:19 +0800

    Adjust RWD layout

--- src/components/TaskGroup.js
+++ src/components/TaskGroup.js
@@ -6,12 +6,12 @@ class TaskGroup extends React.Component {
     let list = []
     let showMore = ''
 
-    if (taskList.length > 3) {
-      taskList = taskList.slice(0, 3)
+    if (taskList.length > 4 ) {
+      taskList = taskList.slice(1, 5)
       showMore = <a href="/">more</a>
     } 
 
-    for (let i=0; i<taskList.length; i++) {
+    for (let i=1; i<taskList.length; i++) {
      list.push(
         <li key={taskList[i].id}>{taskList[i].task}
           <i class="material-icons">play_circle_outline</i>
