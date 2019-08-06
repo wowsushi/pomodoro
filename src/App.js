25f11a94c887c6225b6c1d6e4cda984a58f09dab
bettyChen <s9654025@gmail.com>
2019-08-07 06:37:58 +0800

    Fix bug about  losing focus while use route

--- src/App.js
+++ src/App.js
@@ -8,11 +8,127 @@ import Analytics from './containers/Analytics.js'
 import Ringtones from './containers/Ringtones.js'
 
 
+
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      task: '',
+      taskList: [],
+      activating: false,
+      resting: false,
+      count: 1500
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.changeClockState = this.changeClockState.bind(this)
+    this.selectTask = this.selectTask.bind(this)
+  }
+
+  handleChange(e) {
+    const { value } = e.target
+    this.setState({
+      task: value
+    })
+    console.log('tartet', e.target)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    if (!this.state.task) return
+
+    const newTask = {
+      task: this.state.task,
+      id: Date.now()
+    }
+
+    this.setState(prevState => ({
+      taskList: prevState.taskList.concat(newTask),
+      task: '',
+      id: ''
+    }))
+  }
+
+  countDown() {
+    this.setState(state => ({
+      count: state.count - 1
+    }));
+  }
+
+  changeClockState(e) {
+
+    if (this.state.activating || this.state.resting) {
+      // pause
+      clearInterval(this.interval);
+
+    } else {
+      // activating
+      this.interval = setInterval(() => this.countDown(), 1000);
+
+    }
+    this.setState({ activating: !this.state.activating })
+    this.changeTheme()
+  }
+
+  selectTask(id, e) {
+    let { taskList } = this.state
+
+    taskList.forEach((task, index) => {
+      if (task.id === id) {
+        taskList.unshift(...taskList.splice(index, 1))
+
+        this.setState({ count: 1500 })
+
+        if (this.state.activating) {
+          this.changeClockState(e)
+        }
+      }
+    })
+  }
+
+  changeTheme(state) {
+    const clockWrapper = document.querySelector('.clock-wrapper')
+    const clockMain = document.querySelector('.clock-main')
+    const clockBtn = document.querySelector('.clock-btn')
+    const materialIcons = document.querySelector('.clock-btn > .material-icons')
+    const clockDecoration = document.querySelector('.clock-decoration')
+
+    clockWrapper.classList.toggle('sub-theme')
+    clockMain.classList.toggle('main-theme')
+    clockBtn.classList.toggle('sub-theme')
+    materialIcons.innerHTML =  (materialIcons.innerHTML === 'play_arrow') ? 'pause' : 'play_arrow'
+    materialIcons.classList.toggle('main-theme-font')
+    clockDecoration.classList.toggle('sub-theme')
+    console.log('changed')
+  }
+
   render () {
+    const { task,
+            taskList,
+            activating,
+            resting,
+            count } = this.state
+
     return (
       <HashRouter>
-        <Route exact path="/" component={Home} />
+        <Route exact
+          path="/"
+          render={(props) => (
+            <Home
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              countDown={this.countDown}
+              changeClockState={this.changeClockState}
+              selectTask={this.selectTask}
+              task={task}
+              taskList={taskList}
+              activating={activating}
+              resting={resting}
+              count={count}
+            />
+          )}
+
+        />
         <Route path="/todos" component={Todos} />
         <Route path="/analytics" component={Analytics} />
         <Route path="/ringtones" component={Ringtones} />
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,99 +5,17 @@ import AddTask from '../components/AddTask.js'
 import TaskGroup from '../components/TaskGroup.js'
 import Clock from '../components/Clock.js'
 
-function changeTheme(state) {
-  const clockWrapper = document.querySelector('.clock-wrapper')
-  const clockMain = document.querySelector('.clock-main')
-  const clockBtn = document.querySelector('.clock-btn')
-  const materialIcons = document.querySelector('.clock-btn > .material-icons')
-  const clockDecoration = document.querySelector('.clock-decoration')
-
-  clockWrapper.classList.toggle('sub-theme')
-  clockMain.classList.toggle('main-theme')
-  clockBtn.classList.toggle('sub-theme')
-  materialIcons.innerHTML =  (materialIcons.innerHTML === 'play_arrow') ? 'pause' : 'play_arrow'
-  materialIcons.classList.toggle('main-theme-font')
-  clockDecoration.classList.toggle('sub-theme')
-}
-
 class Home extends React.Component {
   constructor(props) {
     super(props)
     this.state={
-      task: '',
-      taskList: [],
-      activating: false,
-      resting: false,
-      countDown: 1500
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.changeClockState = this.changeClockState.bind(this)
-    this.selectTask = this.selectTask.bind(this)
-  }
-
-  handleChange(e) {
-    const { value } = e.target
-    this.setState({
-      task: value
-    })
-  }
-
-  handleSubmit(e) {
-    e.preventDefault()
-    if (!this.state.task) return
-
-    const newTask = {
-      task: this.state.task,
-      id: Date.now()
-    }
-
-    this.setState(prevState => ({
-      taskList: prevState.taskList.concat(newTask),
-      task: '',
-      id: ''
-    }))
-  }
-
-  countDown() {
-    this.setState(state => ({
-      countDown: state.countDown - 1
-    }));
-  }
-
-  changeClockState(e) {
-
-    if (this.state.activating || this.state.resting) {
-      // pause
-      clearInterval(this.interval);
-
-    } else {
-      // activating
-      this.interval = setInterval(() => this.countDown(), 1000);
 
     }
-    this.setState({ activating: !this.state.activating })
-    changeTheme()
-  }
-
-  selectTask(id, e) {
-    let { taskList } = this.state
-
-    taskList.forEach((task, index) => {
-      if (task.id === id) {
-        taskList.unshift(...taskList.splice(index, 1))
-
-        this.setState({ countDown: 1500 })
-
-        if (this.state.activating) {
-          this.changeClockState(e)
-        }
-      }
-    })
   }
 
   render() {
-    const { taskList, countDown } = this.state
+    console.log(this.props)
+    const { taskList, count } = this.props
 
     let showActivityTask = []
     if (taskList.length > 0) {
@@ -106,12 +24,12 @@ class Home extends React.Component {
           <span class="circle-lg main-decoration"></span>
           <h3>{taskList[0].task}</h3>
           <span class="circle-sm sub-decoration"></span>
-          <div>{ Math.floor(countDown / 60).toString().padStart(2, '0') }
-                :{ (countDown % 60).toString().padStart(2, '0')}
+          <div>{ Math.floor(count / 60).toString().padStart(2, '0') }
+                :{ (count % 60).toString().padStart(2, '0')}
           </div>
         </div>
       )
-      this.clock = <Clock changeClockState={this.changeClockState}/>
+      this.clock = <Clock changeClockState={this.props.changeClockState}/>
     } else {
       showActivityTask.push(
         <div class="activity-task" key={Date.now()}>
@@ -126,15 +44,15 @@ class Home extends React.Component {
     <div class="container">
       <div class="main-panel">
         <AddTask
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-          changeClockState={this.changeClockState}
-          task={this.state.task}
+          handleChange={this.props.handleChange}
+          handleSubmit={this.props.handleSubmit}
+          changeClockState={this.props.changeClockState}
+          task={this.props.task}
         />
         {showActivityTask}
         <TaskGroup
           taskList={taskList}
-          selectTask={this.selectTask}
+          selectTask={this.props.selectTask}
         />
       </div>
       <div class="sub-panel">
